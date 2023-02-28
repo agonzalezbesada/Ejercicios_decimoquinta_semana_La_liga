dda88f0c42f35786af18799b35737a93b51b216a
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-02-28 19:20:28 +0100

    Liga 1.0

---
+++ src/Equipo.java
@@ -0,0 +1,17 @@
+public class Equipo {
+    private String nombreEquipo;
+
+    Equipo() {
+
+    }
+
+    Equipo(String nombre) {
+        this.nombreEquipo=nombre;
+    }
+
+
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner guardar = new Scanner(System.in);
+
+        System.out.println("Qué desea hacer?"+"\n"+"1. Crear liga"+"\n"+"2. Jugar liga"+"\n"+"3. Salir");
+        int eleccion = guardar.nextInt();
+
+        switch(eleccion) {
+
+            case 1:
+                guardar.nextLine();
+                System.out.println("\n"+"Elija un nombre para la liga");
+                String nombre = guardar.nextLine();
+                Liga liga1 = new Liga(nombre);
+
+
+            break;
+
+
+            case 2:
+                liga1.jugarLiga();
+
+            break;
+
+
+            default:
+
+        }
+    }
 }
